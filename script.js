e0e7ad17256495ce24ada9e65d46ad924d0375a8
squashjedi <paulandrewlord@gmail.com>
2022-07-24 20:30:02 +0100

    Added buttons for rock, paper and scissors

--- script.js
+++ script.js
@@ -12,38 +12,53 @@ function playRound(playerSelection, computerSelection) {
   computerSelection = capitalise(computerSelection)
 
   if (playerSelection === computerSelection) {
-    playerScore += 0.5
-    return `Draw! You both selected ${playerSelection}`
+    return 0.5
   } else if (playerSelection === 'Rock' && computerSelection === 'Scissors' || playerSelection === 'Paper' && computerSelection === 'Rock' || playerSelection === 'Scissors' && computerSelection === 'Paper') {
-    playerScore += 1
-    return `You win! ${playerSelection} beats ${computerSelection}`
+    return 1
   } else {
-    return `You lose! ${playerSelection} loses ${computerSelection}`
+    return 0
   }
+  roundStatus.textContent = round
+  round += 1
 }
 
 function game() {
-  for (let i = 0; i < 5; i++) {
-    let computerSelection = getComputerChoice()
-    let playerSelection = prompt('What is you choice... rock, paper or scissors?')
+  let score = [0, 0]
+  const buttons = document.querySelectorAll('button')
+  const message = document.getElementById('message')
+  const currentScore = document.getElementById('current-score')
+  currentScore.textContent = `${score[0]} - ${score[1]}`
 
-    console.log(playRound(playerSelection, computerSelection), playerScore)
-  }
-
-  let outcome
-  let scoreline = playerScore + '-' + (5 - playerScore)
-
-  if (playerScore > 2.5) {
-    outcome = 'Well done... you win!'
-  } else if (playerScore === 2.5) {
-    outcome = 'Drawn game!'
-  } else {
-    outcome = 'Unlucky punk... you lost!'
-  }
-
-  console.log(`${outcome} ${scoreline}`)
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      let playerSelection = button.textContent
+      let computerSelection = getComputerChoice()
+      let result = playRound(playerSelection, computerSelection)
+      if (result === 0.5) {
+        score[0] += 0.5
+        score[1] += 0.5
+        message.textContent = `Draw! You both selected ${computerSelection}.`
+      } else if (result === 1) {
+        score[0] += 1
+        message.textContent = `Won! ${playerSelection} beats ${computerSelection}.`
+      } else {
+        score[1] += 1
+        message.textContent = `Lost! ${playerSelection} loses to ${computerSelection}.`
+      }
+      currentScore.textContent = `${score[0]} - ${score[1]}`
+      if (score[0] >= 5 || score[1] >= 5) {
+        if (score[0] >= 5) {
+          message.textContent = `You won! ${score[0]} - ${score[1]}`
+        } else {
+          message.textContent = `You lost! ${score[0]} - ${score[1]}`
+        }
+        currentScore.textContent = ""
+        buttons.forEach((button) => {
+          button.style.display = "none"
+        })
+      }
+    })
+  })
 }
 
-let playerScore = 0
-
-console.log(game())
+game()
